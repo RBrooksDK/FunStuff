7ef5324b8fcddbf8d4ccbb35a0defdbd43360362
RBrooksDK <richardbrooks.dk@gmail.com>
2024-04-08 15:33:36 +0200

    tried to fix stuff

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -13,10 +13,10 @@ for sheet_name in xls.sheet_names:
     
     # Specify the path and name for the CSV file you want to create
     # This example saves each CSV in a specific directory, with the sheet name as the file name
-    csv_file_path = f'C:/Users/Richard/OneDrive - ViaUC/Arbejde/GitHub/FunStuff/{sheet_name}.csv'
+    #csv_file_path = f'C:/Users/Richard/OneDrive - ViaUC/Arbejde/GitHub/FunStuff/{sheet_name}.csv'
     
     # Save the DataFrame to CSV
-    df.to_csv(csv_file_path, index=False)  # Set index=False if you don't want the DataFrame index in your CSV
+    #df.to_csv(csv_file_path, index=False)  # Set index=False if you don't want the DataFrame index in your CSV
 
-print("CSV files have been created for each sheet.")
-
+#print("CSV files have been created for each sheet.")
+print(df)
